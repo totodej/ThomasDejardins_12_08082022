dddd4cb3ffdb76141b86885c88a065830ccddc73
Thomas Dejardins <tdejardins@gmail.com>
2022-08-08 23:45:38 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  </Router>
+);
