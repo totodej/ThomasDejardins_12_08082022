ac7e51d56fd97b1f1e05284a3ad1eea2a292dabe
Thomas Dejardins <tdejardins@gmail.com>
2022-09-06 12:49:30 +0200

    feat(Error.jsx): add error container

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,18 @@
+import "../styles/pages/Error.css";
+import { Link } from "react-router-dom";
+
+// Error page to show error message
+
 function Error() {
-  return <h1>Error</h1>;
+  return (
+    <div className="error">
+      <h2>404</h2>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="link-error">
+        Retourner sur la page d'acceuil
+      </Link>
+    </div>
+  );
 }
 
 export default Error;
