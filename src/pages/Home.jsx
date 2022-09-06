362a6d61d0757d06c31af79b926be92d5fefb7b7
Thomas Dejardins <tdejardins@gmail.com>
2022-09-06 12:44:53 +0200

    feat(Home.jsx): add home container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import "../styles/pages/Home.css";
+import { USER_MAIN_DATA } from "../data/mocks";
+import { NavLink } from "react-router-dom";
+
+// Home page to show the users list
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <h1>Bienvenue sur SportSee, veuillez selectionner un utilisateur :</h1>
+      <nav>
+        {USER_MAIN_DATA.map((user) => (
+          <NavLink to={"/user/" + user.id} key={user.id}>
+            {user.userInfos.firstName + " " + user.userInfos.lastName}
+          </NavLink>
+        ))}
+      </nav>
+    </div>
+  );
 }
 
 export default Home;
