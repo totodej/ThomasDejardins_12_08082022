8e3e13dfdc5fd9e63b9862ab9e1cd5e5399a4d41
Thomas Dejardins <tdejardins@gmail.com>
2022-09-06 12:59:49 +0200

    feat(index.jsx): add user page

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
+import User from "./pages/User";
 import Error from "./pages/Error";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
+    <Header />
+    <SideBar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/user/:id" element={<User />} />
       <Route path="*" element={<Error />} />
     </Routes>
   </Router>
