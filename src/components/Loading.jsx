1791ff056c010c00498ba9c98fc85b48a7c28b1d
Thomas Dejardins <tdejardins@gmail.com>
2022-09-06 12:54:30 +0200

    feat(Loading.jsx): add loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import "../styles/components/Loading.css";
+
+//Component that build the loading page
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading"></div>
+    </div>
+  );
+}
+
+export default Loading;
