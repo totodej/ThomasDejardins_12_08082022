55a4f50c6e5c29c493aaf93aad6dd7e77aad2bce
Thomas Dejardins <tdejardins@gmail.com>
2022-09-06 12:30:00 +0200

    feat(Header.jsx): add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo/logo.png";
+import "../styles/components/Header.css";
+
+//Component that build the header and navigation
+
+function Header() {
+  return (
+    <div className="header">
+      <img src={logo} alt="logo SportSee" />
+      <nav>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="#">Profil</NavLink>
+        <NavLink to="#">Réglage</NavLink>
+        <NavLink to="#">Communauté</NavLink>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
